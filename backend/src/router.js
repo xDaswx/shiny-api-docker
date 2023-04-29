4ae43a089774cf42f1feb900aaaa1c29689efa9e
sirguy12 <ryankairo09@yahoo.com.br>
2023-04-29 00:47:05 -0300

    Router: endpoint,middeware(validators),function controller

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const animeController = require('./controllers/animeController')
+const middlewares = require('./middlewares/animeMiddle')
 
 const router = express.Router();
 
 router.get('/animes', animeController.getAllAnimes)
+router.post('/animes', middlewares.validador,animeController.createNewAnime)
+router.post('/animes/:id', middlewares.validador,animeController.editAnime)
+router.delete('/animes/:id', animeController.deleteAnime)
 
 
 module.exports = router
