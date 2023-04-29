21edcd14df0418f4861ef9b44f7a08ef30ef7a71
sirguy12 <ryankairo09@yahoo.com.br>
2023-04-29 00:45:17 -0300

    Added animeModels: db querys basicaly

--- backend/src/models/animesModel.js
+++ backend/src/models/animesModel.js
@@ -1,15 +1,53 @@
 const db = require('./dbconnection')
 
 const getAllAnimes = async () => {
-    const result = await db.query('SELECT * FROM ANIMES')
-    return result
+    try{
+        const result = await db.execute('SELECT * FROM assistidos')
+        return result
+    }
+    catch (err){
+        return {message:'DASW DEU ERRO NO SERVIDOR PELO AMOR DE DEUS', log:err}
+    }
+    
 }
 
-const newAnime = () => {
-    return 0
+const newAnime = async (anime) => {
+    try{
+        const date = new Date(Date.now()).toUTCString()
+        const {title, nota, quando} = anime
+        const result = await db.execute('INSERT INTO assistidos (title,nota,quando,adicionado_dia) VALUE (?,?,?,?)', [title,nota,quando,date])
+        return result
+    }
+    catch (err){
+        return {message:'DASW DEU ERRO NO SERVIDOR PELO AMOR DE DEUS', log:err}
+    }
 }
+
+const deleteAnime = async (id) => {
+    try{
+        const result = await db.execute('DELETE FROM assistidos WHERE id = ?', [id])
+        return result
+    }
+    catch (err){
+        return {message:'DASW DEU ERRO NO SERVIDOR PELO AMOR DE DEUS', log:err}
+    }
+}
+
+const editAnime = async (id,json_data) => {
+    try{
+        const {title, nota, quando} = json_data
+        const result = await db.execute('UPDATE assistidos SET title = ?, nota = ?, quando = ? WHERE id = ?', [title,nota,quando,id])
+        return result
+    }
+    catch (err){
+        return {message:'DASW DEU ERRO NO SERVIDOR PELO AMOR DE DEUS', log:err}
+    }
+}
+
 
 module.exports = {
     getAllAnimes,
-    newAnime
+    newAnime,
+    deleteAnime,
+    editAnime
 }
